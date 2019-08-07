ea2e57f9dea66f8abcb1a1afa63787b146e4a390
mofcplcls <zbrogdom@gmail.com>
2019-08-07 20:14:17 +0200

    Test commit
    ...

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+import {Logo, Links, Ads, Footer} from "./visuals.jsx"
 
 
 //https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb/5624139
@@ -22,53 +22,7 @@ function hexToRgb(hex)
     
 }
 
-const Logo = (props) =>
-{
-    return(   
-        <div className="area" id="logo">
-            <div className="card">
-                <div><span>CSS</span>PatternApp</div>
-            </div>
-        </div>
-    );
-}
 
-const Links = (props) =>
-{
-    return(   
-        <div className="area" id="links">
-            <div className="card">
-
-            </div>
-        </div>
-    );
-}
-
-const Ads = (props) =>
-{
-    return(   
-        <div className="area" id="ads">
-
-                <div>Option 1</div>
-                <div>Option 2</div>
-                <div>Option 3</div>
-                <div>Option 4</div>
-                <div>Option 5</div>
-    
-        </div>
-    );
-}
-
-const Footer = (props) =>
-{
-    return(   
-        <div className="area" id="footer">
-            <div>CSSPatternApp® All rights reserved</div>
-            <div>bla bla bla</div>
-            <div>contact@csspatternapp.com</div>
-        </div>
-    );
-}
 
 ////////////////////////////////////////////////////////////////////////MAIN CONTROL/////////////////////////////////////////////////////////////////////////
 
